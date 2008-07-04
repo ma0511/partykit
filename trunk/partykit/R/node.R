776a049e1e8c29e53972dcbc7b9125df56464d88
hothorn <hothorn@255f4249-2263-4417-b1ea-8498779e27a7>
2008-07-04 14:23:52 +0000

    more on nodes
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/partykit@23 255f4249-2263-4417-b1ea-8498779e27a7

--- trunk/partykit/R/node.R
+++ trunk/partykit/R/node.R
@@ -1,23 +1,85 @@
 
-new_node <- function(id, split = NULL, kids = NULL, ...) {
+new_node <- function(id, split = NULL, kids = NULL, info = NULL) {
 
     if (!is.integer(id) || length(id) != 1)
         stop(sQuote("id"), " ", "is not an integer")
 
+    if (is.null(split) != is.null(kids)) {
+        stop(sQuote("split"), " ", "and", " ", sQuote("kids"), " ", 
+             "must either both be specified or unspecified")
+    }
+
     if (!is.null(split)) {
         if (!is.list(split) || any(!sapply(split, inherits, "split")))
-            stop(sQuote("split", " ", "is not a list of", " ", sQuote("split"), 
+            stop(sQuote("split"), " ", "is not a list of", " ", sQuote("split"), 
                         " ", "objects")
     }
 
-    if (!(is.integer(kids) | 
-          (is.list(kids) && all(sapply(kids, inherits, "node")))) 
-        || length(kids) < 2)
-        stop(sQuote("kids"), " ", "must be an integer vector or a list of", 
-             " ", sQuote("node"), " ", "objects")
+    if (!is.null(kids)) {
+        if (!(is.integer(kids) | 
+              (is.list(kids) && all(sapply(kids, inherits, "node")))) 
+            || length(kids) < 2)
+            stop(sQuote("kids"), " ", "must be an integer vector or a list of", 
+                 " ", sQuote("node"), " ", "objects")
+    }
 
-    node <- list(id = id, split = split, kids = kids, info = list(...))
+    node <- list(id = id, split = split, kids = kids, info = info)
     class(node) <- "node"
     return(node)
 }
 
+flat2rec <- function(obj) {
+
+    if (!all(sapply(obj, inherits, "node"))
+        || !all(sapply(obj, function(node) is.null(node$kids) | is.integer(node$kids))))
+        stop(sQuote("obj"), " ", "is not a list of flat", " ", 
+             sQuote("node"), " ", "objects")
+    
+    obj <- obj[order(sapply(obj, function(node) node$id))]
+    if (length(obj) == 1) return(obj)
+
+    new_recnode <- function(id) {
+        if (is.null(obj[[id]]$kids))
+            new_node(id = id, info = obj[[id]]$info)
+        else
+            new_node(id = id, split = obj[[id]]$split, 
+                     kids = lapply(obj[[id]]$kids, new_recnode), 
+                     info = obj[[id]]$info)
+    }
+        
+    node <- new_node(id = as.integer(1), split = obj[[1]]$split, 
+                     kids = lapply(obj[[1]]$kids, new_recnode), info = obj[[1]]$info)
+    return(node)
+}
+
+get_node_id <- function(data, node) {
+
+    if (is.null(node$split))
+        return(rep(node$id, nrow(data)))
+    nextid <- do_splitlist(data, node$split)
+    for (i in unique(nextid)) {
+        nextid[nextid == i] <- get_node_id(data[nextid == i, , drop = FALSE], 
+                                           node$kids[[i]])
+    }
+    return(nextid)
+}
+
+rec2flat <- function(node) {
+
+    if (is.null(node$split) && is.null(node$kids))
+        return(list(node))
+
+    obj <- list()
+    
+    new_flatnode <- function(node) {
+        if (is.null(node$kids))
+            obj[[node$id]] <<- new_node(id = node$id, info = node$info)
+        else {
+            obj[[node$id]] <<- new_node(id = node$id, split = node$split,
+                 kids = sapply(node$kids, function(k) k$id), info = node$info)
+            lapply(node$kids, new_flatnode)
+        }
+    }
+    new_flatnode(node)
+    return(obj)
+}
---
+++ trunk/partykit/tests/regtest-node.R
@@ -0,0 +1,16 @@
+
+    
+foo <- function(x) list(new_split(as.integer(1), breaks = as.double(x)))
+x <- vector(mode = "list", length = 3)
+x[[1]] <- new_node(id = 1:1, split = foo(1 / 3), kids = 2:3, info = "one")
+x[[2]] <- new_node(id = 2:2, info = "two")
+x[[3]] <- new_node(id = 3:3, split = foo(2 / 3), kids = 4:5, info = "three")
+x[[4]] <- new_node(id = 4:4, info = "four")
+x[[5]] <- new_node(id = 5:5, info = "five")
+
+rx <- flat2rec(x)
+stopifnot(identical(rec2flat(rx), x))
+
+dat <- data.frame(x = runif(100))
+get_node_id(dat, rx)
+
