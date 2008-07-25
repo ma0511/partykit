4c99eb649702f81588fedfbe2d48fb722f032513
hothorn <hothorn@255f4249-2263-4417-b1ea-8498779e27a7>
2008-07-25 11:54:14 +0000

    + Glaucoma example
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/partykit@37 255f4249-2263-4417-b1ea-8498779e27a7

--- trunk/partykit/tests/regtest-party.R
+++ trunk/partykit/tests/regtest-party.R
@@ -2,7 +2,7 @@
 sapply(dir(path = "../R", pattern = "R$", full = TRUE), source)
 
 library("rpart")
-fit <- rpart(Kyphosis ~ Age + Number + Start, data=kyphosis)
+fit <- rpart(Kyphosis ~ Age + Number + Start, data = kyphosis)
 
 pfit <- as.party(fit)
 
@@ -13,3 +13,15 @@ library("RWeka")
 itree <- J48(Species ~ ., data = iris)
 pitree <- as.party(itree)
 all(predict(pitree) == predict(pitree, newdata = iris[, 3:4]))
+
+
+data("GlaucomaM", package = "ipred")
+
+fit <- rpart(Class ~ ., data = GlaucomaM)
+pfit <- as.party(fit)
+all(do_nodeid(pfit$node, GlaucomaM[, pfit$metadata$varnames]) == fit$where)
+
+itree <- J48(Class ~ ., data = GlaucomaM)
+pitree <- as.party(itree)
+all(predict(pitree) == predict(pitree, newdata = GlaucomaM))
+
