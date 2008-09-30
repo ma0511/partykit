53ac9258c3fc3cd5fd3d0d1bc9f4e06cb44af32d
hothorn <hothorn@255f4249-2263-4417-b1ea-8498779e27a7>
2008-09-30 11:52:57 +0000

    suppressWarnings
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/partykit@96 255f4249-2263-4417-b1ea-8498779e27a7

--- trunk/partykit/R/utils.R
+++ trunk/partykit/R/utils.R
@@ -1,12 +1,6 @@
 
 median_survival_time <- function(x) {
     minmin <- function(y, xx) {
-
-        ### don't complain about Inf
-        ww <- getOption("warn")
-        on.exit(options(warn = ww))
-        options(warn = -1)
-
         if (any(!is.na(y) & y==.5)) {
             if (any(!is.na(y) & y <.5))
                 .5*(min(xx[!is.na(y) & y==.5]) + min(xx[!is.na(y) & y<.5]))
@@ -14,6 +8,6 @@ median_survival_time <- function(x) {
                 .5*(min(xx[!is.na(y) & y==.5]) + max(xx[!is.na(y) & y==.5]))
         } else   min(xx[!is.na(y) & y<=.5])
     }
-    med <- minmin(x$surv, x$time)
+    med <- suppressWarnings(minmin(x$surv, x$time))
     return(med)
 }
