8427d8459b01326da87999c5b7e19b939e207f2a
thothorn <thothorn@255f4249-2263-4417-b1ea-8498779e27a7>
2009-01-24 17:01:41 +0000

    rules for splits in numeric variables
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/partykit@124 255f4249-2263-4417-b1ea-8498779e27a7

--- pkg/R/party.R
+++ pkg/R/party.R
@@ -445,7 +445,13 @@ depth.party <- function(x, ...) {
   depth(node_party(x), ...)
 }
 
-rule <- function(x, i, ...) {
+list.rules.party <- function(x, i = NULL, ...) {
+    if (is.null(i)) i <- nodeids(x, terminal = TRUE)
+    if (length(i) > 1) {
+        ret <- sapply(i, list.rules.party, x = x)
+        names(ret) <- if (is.character(i)) i else names(x)[i]
+        return(ret)
+    }
     if (is.character(i) && !is.null(names(x)))
         i <- which(names(x) %in% i)
     stopifnot(length(i) == 1 & is.numeric(i))
@@ -462,7 +468,6 @@ rule <- function(x, i, ...) {
         fit <- NULL  
         dat <- x$data
     }
-    nam <- names(x)[nodeids(x, from = i, terminal = FALSE)]
 
     rule <- c()
 
@@ -470,14 +475,29 @@ rule <- function(x, i, ...) {
         if (id_node(node) == i) return(NULL)   
         kid <- sapply(kids_node(node), id_node)
         whichkid <- max(which(kid <= i))
-        svar <- names(dat)[node$split$varid]
-        slevels <- levels(dat[, svar])[node$split$index == whichkid]
+        split <- split_node(node)
+        ivar <- varid_split(split)
+        svar <- names(dat)[ivar]
+        index <- index_split(split)
         if (is.factor(dat[, svar])) {
+            slevels <- levels(dat[, svar])[index == whichkid]
             srule <- paste(svar, " %in% c(\"", 
                 paste(slevels, collapse = "\", \"", sep = ""), "\")",
                 sep = "")
         } else {
-            stop("not yet implemented")
+            if (is.null(index)) index <- 1:length(kid)
+            breaks <- cbind(c(-Inf, breaks_split(split)), 
+                            c(breaks_split(split), Inf))
+            sbreak <- breaks[index == whichkid,]
+            right <- right_split(split)
+            srule <- c()
+            if (is.finite(sbreak[1]))
+                srule <- c(srule, 
+                    paste(svar, ifelse(right, ">", ">="), sbreak[1]))
+            if (is.finite(sbreak[2]))
+                srule <- c(srule, 
+                    paste(svar, ifelse(right, "<=", ">"), sbreak[2]))
+            srule <- paste(srule, collapse = " & ")
         }
         rule <<- c(rule, srule)
         return(recFun(node[[whichkid]]))
