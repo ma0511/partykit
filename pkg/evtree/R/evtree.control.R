534e41fad52884e5c60357dade93b6e6269df053
azeileis <azeileis@255f4249-2263-4417-b1ea-8498779e27a7>
2011-06-27 12:32:47 +0000

    streamlined evtree.control
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/partykit@192 255f4249-2263-4417-b1ea-8498779e27a7

--- pkg/evtree/R/evtree.control.R
+++ pkg/evtree/R/evtree.control.R
@@ -1,117 +1,67 @@
-evtree.control <- function(minbucket = 7L, minsplit = 20L, maxdepth = 9L, niterations = 10000L, ntrees = 100L, alpha = 1,
-  operatorprob= list(pmutatemajor = 0.2, pmutateminor = 0.2, pcrossover = 0.2, psplit = 0.2, pprune = 0.2), seed = NULL, ...){
-    args <- list(...)
-    if( length(args) > 0 ){   
-	for(a in 1:length(args))
-		if(names(args)[a] == "minbucket")
-			minbucket <- args[[a]]
-		else if(names(args)[a] == "minsplit")
-			minsplit <- args[[a]]
-		else if(names(args)[a] == "maxdepth")
-			maxdepth <- args[[a]]
-		else if(names(args)[a] == "niterations")
-			niterations <- args[[a]]
-		else if(names(args)[a] == "ntrees")
-			ntrees <- args[[a]]
-		else if(names(args)[a] == "alpha")
-			alpha <- args[[a]]
-		else if(names(args)[a] == "seed")
-			seed <- args[[a]]
-		else if(names(args)[a] == "operatorprob"){
-		  	if(is.list(args)[a]){
-		  	          op <- args[[a]]
-		  	      for(i in 1:length(op))
-			      if(names(op)[a] == "pmutatemajor")
-		    	          operatorprob$pmutatemajor <- op[i]
-			      else if(names(op)[a] == "pmutateminor")
-			          operatorprob$pmutateminor <- op[i]
-			      else if(names(op)[a] == "pcrossover")
-				  operatorprob$pcrossover <- op[i]
-			      else if(names(op)[a] == "psplit")
-				  operatorprob$psplit <- op[i]
-			      else if(names(op)[a] == "pprune")
-			          operatorprob$pprune <- op[i]	  	      
-		  	      else
-			          warning(paste("extra argument ", names(op)[i], " in \"operatorprob\" is just disregarded"))
-		  	}else if(length(args)[a] == 5){
-  		  	      op <- list()
-    	  		      op$pmutatemajor <- operatorprob[[1]]
-    	    		      op$pmutateminor <- operatorprob[[2]]
-    	     	  	      op$psplit <- operatorprob[[3]]
-    	         	      op$pprune <- operatorprob[[4]]
-    	          	      op$pcrossover <- operatorprob[[5]]
-    	          	      operatorprob <- op
-			}else{
-   	   		      stop("argument operatorprob must be of form: operatorprob= list(pmutatemajor = 0.2, pmutateminor = 0.2, pcrossover = 0.2, psplit = 0.2, pprune = 0.2), or a vector with 5 elements")
-   	   	        }
-   	        }
-		else if(names(args)[a] == "pmutatemajor")
-			operatorprob$pmutatemajor <- args[[a]]
-		else if(names(args)[a] == "pmutateminor")
-			operatorprob$pmutateminor <- args[[a]]
-		else if(names(args)[a] == "pcrossover")
-			operatorprob$pcrossover <- args[[a]]
-		else if(names(args)[a] == "psplit")
-			operatorprob$psplit <- args[[a]]
-		else if(names(args)[a] == "pprune")
-			operatorprob$pprune <- args[[a]]
-		else
-			warning(paste("extra argument ", names(args)[a], " is just disregarded"))
-     }
-  
-      if( is.null(operatorprob$pmutatemajor) )
-  	   operatorprob$pmutatemajor <- 0.2
-      if( is.null( operatorprob$pmutateminor) )
-            operatorprob$pmutateminor<- 0.2
-      if( is.null( operatorprob$pprune) )
-	    operatorprob$pprune<- 0.2
-      if( is.null(operatorprob$pcrossover) )
-    	    operatorprob$pcrossover<- 0.2
-      if( is.null( operatorprob$psplit) )
-  	    operatorprob$psplit<- 0.2
- 
+evtree.control <- function(minbucket = 7L, minsplit = 20L, maxdepth = 9L,
+  niterations = 10000L, ntrees = 100L, alpha = 1,
+  operatorprob = list(pmutatemajor = 0.2, pmutateminor = 0.2, pcrossover = 0.2, psplit = 0.2, pprune = 0.2),
+  seed = -1L, ...)
+{
+  minbucket <- as.integer(minbucket)
+  if(minbucket < 1L) {
+    warning("parameter \"minbucket\" must be positive, default used instead")
+    minbucket <- 7L
+  }
 
-      sumop <- operatorprob$pmutatemajor + operatorprob$pmutateminor + operatorprob$pprune + operatorprob$pcrossover + operatorprob$psplit
-      if(sumop != 1 && sumop != 100)      
-             warning('user defined operator probabilities do not sum up to 1')
-      operatorprob$pmutatemajor <- floor(100* operatorprob$pmutatemajor / sumop)
-      operatorprob$pmutateminor <- floor(100* operatorprob$pmutateminor / sumop)
-      operatorprob$pprune <-  floor(100*operatorprob$pprune / sumop)
-      operatorprob$pcrossover <-  floor(100*operatorprob$pcrossover  / sumop)
-      operatorprob$psplit <- 100 - (operatorprob$pmutatemajor + operatorprob$pmutateminor + operatorprob$pprune + operatorprob$pcrossover)
-  	    	     
-      if(minsplit < 2 | minbucket < 1 )
-             stop("parameters \"minsplit\" and \"minbucket\" must be defined with at least 1 and 2 observations")
+  minsplit <- as.integer(minsplit)
+  if(minsplit < 2 * minbucket) {
+    warning("parameter \"minsplit\" must be at least twice as large as \"minbucket\", changed")
+    minsplit <- 2 * minbucket
+  }
 
-      if(minsplit < minbucket)
- 	     minsplit <- minbucket+1
+  maxdepth <- as.integer(maxdepth)
+  if(maxdepth > 12) {
+    warning("computations may take extremely long for \"maxdepth\" > 12 (or even be infeasible)")
+  }
 
-      if(is.null(seed)){
-   	     seed = -1
-     }else if(seed < 0){
-  	     stop("parameter \"seed\" in evtree.control must be a non-negative integer")
-     }
+  if(!is.integer(niterations)) seed <- as.integer(niterations)
+  if(niterations < 100L) {
+    warning("computations may be unreliable for \"niterations\" < 100")
+  }
 
-     if(niterations < 100)
-             stop("parameter \"niterations\" must at least be 100")
+  if(!is.integer(ntrees)) seed <- as.integer(ntrees)
+  if(niterations < 10L) {
+    warning("computations may be unreliable for \"ntrees\" < 10")
+  }
 
-     if(ntrees < 10)
-             stop("parameter \"ntrees\" must at least be 10")
+  alpha <- as.numeric(alpha)
+  if(alpha < 0) {
+    warning("parameter \"alpha\" must be equal or larger than 0, default used")
+    alpha <- 1
+  }
 
-     if(alpha < 0)
-             stop("parameter \"alpha\" must be equal or larger than 0")
+  if(!is.integer(seed)) seed <- as.integer(seed)
+  if(seed < -1L) {
+    warning("parameter \"seed\" must be non-negative, default used instead")
+    seed <- -1L
+  }
 
-     if(maxdepth > 12)
-            stop("\"maxdepth > 12\" is not supported")
-  
-     list(minbucket = minbucket,
-   	    minsplit = minsplit,
-   	    maxdepth = maxdepth,
-   	    niterations = niterations,
-            ntrees = ntrees,
-            alpha = alpha,
-            operatorprob = operatorprob,
-	    seed = seed
-        )
+  op <- c(pmutatemajor = 0.2, pmutateminor = 0.2, pcrossover = 0.2, psplit = 0.2, pprune = 0.2)
+  operatorprob <- unlist(operatorprob)
+  if(is.null(names(operatorprob)) & length(operatorprob) == 5L) names(operatorprob) <- names(op)
+  if(is.null(names(operatorprob))) {
+    warning("incorrect specification of \"operatorprob\", default used")
+    operatorprob <- op
+  }
+  op[names(operatorprob)] <- operatorprob
+  operatorprob <- op/sum(op)
+
+
+  list(
+    minbucket = minbucket,
+    minsplit = minsplit,
+    maxdepth = maxdepth,
+    niterations = niterations,
+    ntrees = ntrees,
+    alpha = alpha,
+    operatorprob = operatorprob,
+    seed = seed
+  )
 }
 
