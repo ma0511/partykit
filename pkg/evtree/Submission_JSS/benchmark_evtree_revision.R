a03e451a7a6f7da95b112a4e176ef597adb45941
thomasgrubinger <thomasgrubinger@255f4249-2263-4417-b1ea-8498779e27a7>
2013-04-29 20:08:59 +0000

    added plot functionality
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/partykit@306 255f4249-2263-4417-b1ea-8498779e27a7

--- pkg/evtree/Submission_JSS/benchmark_evtree_revision.R
+++ pkg/evtree/Submission_JSS/benchmark_evtree_revision.R
@@ -353,24 +353,160 @@ bootstrap_evtree_parameter <- function(kdata=1, nboots = 50, seed = 1000){
 data(StatlogHeart)
 rheart <- bootstrap_evtree_parameter(StatlogHeart, nboots = 250)
 apply(apply(rheart, 2, as.numeric),2,mean)
-save(rheart, file="results/heart_parameter.RData")
+save(rheart, file="results2/heart_parameter.RData")
 
 ### 8. statlogGermanCredit ###
 data(GermanCredit)
 rcredit <- bootstrap_evtree_parameter(GermanCredit, nboots = 250)
 apply(apply(rcredit, 2, as.numeric),2,mean)
-save(rcredit, file="results/credit_parameter.RData")
+save(rcredit, file="results2/credit_parameter.RData")
 
 ### 11. spam ###
 data(spam)
 rspam <- bootstrap_evtree_parameter(spam, nboots = 250)
 apply(apply(rspam, 2, as.numeric),2,mean)
-save(rspam, file="results/spam_parameter.RData")
+save(rspam, file="results2/spam_parameter.RData")
 
 ### 3. 4x4 chessboard with 5% noise ###
 rchessboard44_5 <- bootstrap_evtree_parameter(kdata = 1, nboots = 250)
 apply(apply(rchessboard44_5, 2, as.numeric),2,mean)
-save(rchessboard44_5, file="results/chessboard44_5_parameter.RData")
+save(rchessboard44_5, file="results2/chessboard44_5_parameter.RData")
+
+###################################################
+### Visualisation of datasets
+###################################################
+library("plotrix")
+library("lme4")
+library("multcomp")
+
+load("results2/heart_parameter.RData")
+load("results2/credit_parameter.RData")
+load("results2/spam_parameter.RData")
+load("results2/chessboard44_5_parameter.RData")
+
+
+panel.mean <- function(x,y,...){
+	x <- as.numeric(x)
+	x.unique <- unique(x)
+	for(X in x.unique) {
+		Y <- y[x == X]
+		if (!length(Y)) next
+		mean.value <- list(y = mean(Y), x = X)
+		do.call("lpoints", c(mean.value, pch = 20, col= "red"))
+	}
+}
+
+# functions for the visualisation of different operatorprobabilities 
+
+preprocess_op <- function(d, dname = "datasetname"){
+	d <- as.data.frame(d)
+    colnames(d) <- c("c0m50sp50", "c20m40sp40","c40m30sp30","c20m20sp60","c20m60sp20")
+    x <- d*100
+    x <- reshape(x, idvar="samp", times=names(x), timevar = "operatorprob",varying= list(names(x)), direction="long")
+	names(x)[[2]] <- "value"
+	x$ds <- dname
+    return(x)
+}
+
+preprocess_op_comp <- function(ntrees, colum){	
+	rt <- preprocess_op(d = rheart[,colum], dname = "Statlog heart")
+	rt <- rbind(rt, preprocess_op(d = rcredit[,colum], dname = "Statlog German credit"))
+	rt <- rbind(rt, preprocess_op(d = rspam[,colum], dname = "Spam"))
+	rt <- rbind(rt, preprocess_op(d = rchessboard44_5[,colum], dname = "Chessboard 4x4 with 5% noise"))
+	rt <- cbind(rt, rep( ntrees, dim(rt)[1]))
+	colnames(rt)[5] <- "nIter"
+	rt
+}
+
+r2 <- preprocess_op_comp("200 iterations", c(11:15))
+r2 <- rbind(r2, preprocess_op_comp("500 iterations", c(16:20)))
+r2 <- rbind(r2, preprocess_op_comp("10000 iterations", c(21:25)))
+
+
+sort_op <- function(x){
+	x$ds <- relevel(x$ds, "Statlog heart")
+	x$ds <- relevel(x$ds, "Statlog German credit")
+	x$ds <- relevel(x$ds, "Spam")
+	x$ds <- relevel(x$ds, "Chessboard 4x4 with 5% noise")
+	x
+}
+
+r2$operatorprob <- factor(r2$operatorprob)
+r2$ds <- factor(r2$ds)
+r2$nIter <- factor(r2$nIter)
+r2 <- sort_datasets(r2)
+
+pdf(file= "~/Desktop/evtree_ntrees.pdf", width=10.3, height=20.45)
+b1 <- bwplot (value ~ factor(operatorprob)| nIter+ds , data= as.data.frame(r2), 
+horizontal = FALSE,  
+ylab= list("Accuracy [%]", cex=1.1),
+pch= '|',
+scales= list(x= list(rot=60), y= list(at = c(50, 60, 70, 80, 90, 100),  alternating = F)), 
+ylim=c(55,100), 
+layout = c(3,4),
+panel=function(x,y,...) {
+	panel.abline(h=c(50, 60, 70, 80, 90, 100), lwd = 1, lty = 2, col = "gray27")
+	panel.bwplot(x,y,...)
+	panel.mean(x,y,...)
+}
+)
+b1
+dev.off()
+
+
+# functions for the visualisation of different population sizes 
+preprocess_ntrees <- function(d, dname = "datasetname"){
+	d <- as.data.frame(d)
+    colnames(d) <- c("25 trees", "50 trees","100 trees","250 trees","500 trees")
+    x <- d*100
+    x <- reshape(x, idvar="samp", times=names(x), timevar = "ntrees",varying= list(names(x)), direction="long")
+    names(x)[[2]] <- "value"
+	x$ds <- dname
+    return(x)
+}
+
+r <- preprocess_ntrees(d = rheart[,1:5], dname = "Statlog heart")
+r <- rbind(r, preprocess_ntrees(d = rcredit[,1:5], dname = "Statlog German credit"))
+r <- rbind(r, preprocess_ntrees(d = rspam[,1:5], dname = "Spam"))
+r <- rbind(r, preprocess_ntrees(d = rchessboard44_5[,1:5], dname = "Chessboard 4x4 with 5% noise"))
+
+sort_ntrees <- function(x){
+	x$ds <- relevel(x$ds, "Chessboard 4x4 with 5% noise")
+	x$ds <- relevel(x$ds, "Spam")
+	x$ds <- relevel(x$ds, "Statlog German credit")
+    x$ds <- relevel(x$ds, "Statlog heart")
+	x$ntrees <- relevel(x$ntrees, "250 trees")
+	x$ntrees <- relevel(x$ntrees, "100 trees")
+	x$ntrees <- relevel(x$ntrees, "50 trees")
+	x$ntrees <- relevel(x$ntrees, "25 trees")
+	x
+}
+
+r$ntrees <- factor(r$ntrees) 
+r$ds <- factor(r$ds)
+r <- sort_ntrees(r) 
+par.settings = list(cex=1.2)
+pdf(file= "~/Desktop/evtree_op.pdf", width=10.3, height=5.6)
+b1 <- bwplot (value ~ ntrees | ds, data= as.data.frame(r), 
+horizontal = FALSE,  
+ylab= list("Accuracy [%]", cex=1.1),
+pch= '|',
+scales= list(x= list(rot=60), y= list(at = c(50, 60, 70, 80, 90, 100),  alternating = F)), 
+ylim=c(55,100), 
+layout = c(4,1),
+panel=function(x,y,...) {
+      #  panel.abline(h=1, lwd= 1, lty= 5)
+        panel.abline(h=c(0, 0.5, 1.5, 2, 3, 4), lwd = 1, lty = 2, col = "gray27")
+        panel.bwplot(x,y,...)
+        panel.mean(x,y,...)
+       }
+)
+b1
+dev.off()
+
+
+
+
 
 
 
