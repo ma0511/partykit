d174b411923a54895f5aa6ba42bdb53ac1abea8d
azeileis <azeileis@255f4249-2263-4417-b1ea-8498779e27a7>
2014-03-21 15:00:21 +0000

    first steps towards mixforest
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/partykit@517 255f4249-2263-4417-b1ea-8498779e27a7

--- pkg/partykit/R/meanvartree.R
+++ pkg/partykit/R/meanvartree.R
@@ -13,7 +13,7 @@ meanvarfit <- function(y, x = NULL, start = NULL, weights = NULL, offset = NULL,
   )
 }
 
-meanvartree <- function(formula, data, na.action, weights, subset, minsplit = 7L, ...)
+meanvartree <- function(formula, data, na.action, weights, subset, minsplit = 10L, ...)
 {
   ## keep call
   cl <- match.call(expand.dots = TRUE)
@@ -39,13 +39,72 @@ meanvartree <- function(formula, data, na.action, weights, subset, minsplit = 7L
   return(rval)
 }
 
+mixforest <- function(formula, data, na.action, weights, subset,
+  ntree = 500L, mtry = 3L, minsplit = 10L, replace = TRUE, fraction = 0.632, ...)
+{
+  ## call and formula
+  cl <- match.call()
+  if(missing(data)) data <- environment(formula)
+  mf <- match.call(expand.dots = FALSE)
+  m <- match(c("formula", "data", "subset", "na.action", "weights"), names(mf), 0)
+  mf <- mf[c(1L, m)]
+  mf$drop.unused.levels <- TRUE
+  
+  ## call model.frame()
+  mf[[1]] <- as.name("model.frame")
+  mf <- eval(mf, parent.frame())
+  n <- nrow(mf)
+  
+  ## extract terms, model matrices, response, weights
+  mt <- terms(formula, data = data)
+  y <- mf[[1L]]
+  z <- mf[, -1L, drop = FALSE]
+  w <- model.weights(mf)
+  if(is.null(w)) w <- rep.int(1L, n)
+  z[["(weights)"]] <- NULL
+
+  ## resampling weights (hmm, there must be a more elegant solution...)
+  rw <- matrix(0L, nrow = n, ncol = ntree)
+  for(i in 1:ntree) {
+    tab <- table(sample(1:n, n, replace = TRUE))
+    rw[as.numeric(names(tab)), i] <- as.integer(tab)
+  }
+
+  ## fit trees
+  nodes <- lapply(1:ntree, function(i) partykit:::mob_partynode(Y = y, Z = z, weights = w * rw[, i],
+    fit = meanvarfit, control = mob_control(mtry = mtry, minsplit = minsplit, ...), nyx = 1L))
+
+  list(
+    nodes = nodes,
+    data = mf,
+    weights = rw
+  )
+}
+
 if(FALSE) {
 library("partykit")
 
+## single tree
 airq <- subset(airquality, !is.na(Ozone) & !is.na(Solar.R))
 mv <- meanvartree(Ozone ~ Solar.R + Wind + Temp + Month + Day, data = airq)
 plot(mv)
 coef(mv)
 
 coef(mv)[as.character(predict(mv, type = "node")), ]
+
+
+## forest
+data("BostonHousing", package = "mlbench")
+BostonHousing <- transform(BostonHousing,
+  chas = factor(chas, levels = 0:1, labels = c("no", "yes")),
+  rad = factor(rad, ordered = TRUE))
+
+set.seed(1)
+bh <- mixforest(medv ~ lstat + rm + zn + indus + chas + nox + age + dis + rad + tax + crim + b + ptratio,
+  data = BostonHousing)
+
+plot(party(bh$nodes[[1]], bh$data),
+  tp_args = list(FUN = function(i) paste("n =", i$nobs)))
+plot(party(bh$nodes[[2]], bh$data),
+  tp_args = list(FUN = function(i) paste("n =", i$nobs)))
 }
