ea5c9a0852aa8909ce6d1422c5a5eb403ceb2a6a
azeileis <azeileis@255f4249-2263-4417-b1ea-8498779e27a7>
2014-03-22 15:26:20 +0000

    more ideas for mixforests
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/partykit@518 255f4249-2263-4417-b1ea-8498779e27a7

--- pkg/partykit/R/meanvartree.R
+++ pkg/partykit/R/meanvartree.R
@@ -74,11 +74,13 @@ mixforest <- function(formula, data, na.action, weights, subset,
   nodes <- lapply(1:ntree, function(i) partykit:::mob_partynode(Y = y, Z = z, weights = w * rw[, i],
     fit = meanvarfit, control = mob_control(mtry = mtry, minsplit = minsplit, ...), nyx = 1L))
 
-  list(
+  rval <- list(
     nodes = nodes,
     data = mf,
     weights = rw
   )
+  class(rval) <- "mixforest"
+  return(rval)
 }
 
 if(FALSE) {
@@ -107,4 +109,57 @@ plot(party(bh$nodes[[1]], bh$data),
   tp_args = list(FUN = function(i) paste("n =", i$nobs)))
 plot(party(bh$nodes[[2]], bh$data),
   tp_args = list(FUN = function(i) paste("n =", i$nobs)))
+
+
+data("NPreg", package = "flexmix")
+NPreg$class <- factor(NPreg$class)
+set.seed(1)
+mf1 <- mixforest(yn ~ x,         data = NPreg)
+mf2 <- mixforest(yn ~ x + class, data = NPreg)
+mf <- mf1
+
+plot(party(mf$nodes[[1]], mf$data),
+  tp_args = list(FUN = function(i) paste(c("n", "mean", "log(var)"), "=", round(c(i$nobs, i$coefficients), 3))))
+
+x <- 0:50/5
+nd <- matrix(0, nrow = 51, ncol = 500)
+pm <- matrix(0, nrow = 51, ncol = 500)
+ps <- matrix(0, nrow = 51, ncol = 500)
+d0 <- mf$data[0,]
+d1 <- data.frame(x = x, class = factor(rep(2, 51), levels = 1:2))
+for(i in 1:500) {
+  nd[,i] <- predict(party(mf$nodes[[i]], d0), d1)
+  ndi <- sort(unique(nd[,i]))
+  pari <- do.call("rbind", nodeapply(mf$nodes[[i]], ndi, function(n) n$info$coefficients))
+  rownames(pari) <- ndi
+  pm[,i] <- pari[as.character(nd[,i]), 1]
+  ps[,i] <- sqrt(exp(pari[as.character(nd[,i]), 2]))
+}
+
+plot(yn ~ x, data = NPreg)
+lines(x,  0 +  5 * x + 0 * x^2, lty = 2)
+lines(x, 15 + 10 * x - 1 * x^2, lty = 2)
+lines(x, 7.5 + 7.5 * x - 0.5 * x^2, lty = 3, col = 4)
+## variance would need: p * E(A^2) - p^2 * E(A)^2 + (1-p) * E(B^2) - (1-p)^2 * E(B)^2 - p * (1-p) * E(A) * E(B)
+lines(x, rowMeans(pm), col = 2, lty = 2)
+
+
+mixmean <- function(x) 7.5 + 7.5 * x - 0.5 * x^2
+mixdens <- function(x) {
+  function(y) 0.5 * dnorm(y, mean = 0 +  5 * x + 0 * x^2, sd = 3) + 0.5 * dnorm(y, mean = 15 + 10 * x - 1 * x^2, sd = 3)
+}
+y <- -2:260/5
+plot(y, mixdens(3)(y), type = "l", ylim = c(0, 0.13))
+lines(y, mixdens(5)(y), type = "l", lty = 2, col = 2)
+lines(y, mixdens(7)(y), type = "l", lty = 3, col = 3)
+lines(y, mixdens(9)(y), type = "l", lty = 4, col = 4)
+legend("topleft", as.character(1:4 * 2 + 1), lty = 1:4, col = 1:4, bty = "n")
+
+for(i in 1:4) {
+  m <- pm[6 + i * 10]
+  s <- ps[6 + i * 10]
+  d <- matrix(dnorm(rep(y, each = length(m)), mean = rep(m, length(y)), sd = rep(s, length(y))), ncol = length(y))
+  lines(y, colMeans(d), lty = i, col = i)
+}
+
 }
