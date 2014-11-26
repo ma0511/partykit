288204307aa4a5725d3de0c609f53df183858f94
thothorn <thothorn@255f4249-2263-4417-b1ea-8498779e27a7>
2014-11-26 09:09:40 +0000

    add link to partykit webpage
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/partykit@640 255f4249-2263-4417-b1ea-8498779e27a7

--- www/index.php
+++ www/index.php
@@ -43,7 +43,7 @@ echo $contents; } ?>
 
 <!-- end of project description -->
 
-<p> No content added. </p>
+<p> <A HREF="http://partykit.R-forge.R-project.org/partykit">Package partykit website</A> </p>
 
 <p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
 
