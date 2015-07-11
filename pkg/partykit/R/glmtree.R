c5c6365fface4e3b5cbf54a959cbae2913a53d7c
azeileis <azeileis@255f4249-2263-4417-b1ea-8498779e27a7>
2015-07-11 21:20:57 +0000

    refit.modelparty() now checks whether an intercept is needed in x even if there are no real regressors
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/partykit@727 255f4249-2263-4417-b1ea-8498779e27a7

--- pkg/partykit/R/glmtree.R
+++ pkg/partykit/R/glmtree.R
@@ -60,8 +60,10 @@ glmfit <- function(y, x, start = NULL, weights = NULL, offset = NULL, cluster = 
   }
   args$control <- do.call("glm.control", ctrl)
   
-  ## FIXME: add intercept-only regressor matrix (if missing)
-  if(is.null(x)) x <- matrix(1, nrow = NROW(y), ncol = 1L)
+  ## add intercept-only regressor matrix (if missing)
+  ## NOTE: does not have terms/formula
+  if(is.null(x)) x <- matrix(1, nrow = NROW(y), ncol = 1L,
+    dimnames = list(NULL, "(Intercept)"))
   
   ## call glm fitting function
   args <- c(list(x = x, y = y, start = start, weights = weights, offset = offset), args)
