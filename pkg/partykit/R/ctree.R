514248ee5ac163d3e7caa74c0ed1c281580b4b92
thothorn <thothorn@255f4249-2263-4417-b1ea-8498779e27a7>
2015-08-11 13:38:46 +0000

    coin 1.1-0 has an improved logrank_trafo, use this one
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/partykit@736 255f4249-2263-4417-b1ea-8498779e27a7

--- pkg/partykit/R/ctree.R
+++ pkg/partykit/R/ctree.R
@@ -377,18 +377,23 @@ ctree <- function(formula, data, weights, subset, na.action = na.pass,
     return(tree)
 }
 
-.logrank_trafo <- function(x, ties.method = c("logrank", "HL")) {
-    ties.method <- match.arg(ties.method)
-    time <- x[,1]
-    event <- x[,2]
-    n <- length(time)
-    ot <- order(time, event)
-    rt <- rank(time, ties.method = "max")
-    mt <- rank(time, ties.method = "min") - 1
-    fact <- switch(ties.method, "logrank" = event / (n - mt),
-                                "HL" = event/(n - rt + 1)
-                  )   
-    event - cumsum(fact[ot])[rt]
+#.logrank_trafo <- function(x, ties.method = c("logrank", "HL")) {
+#    ties.method <- match.arg(ties.method)
+#    time <- x[,1]
+#    event <- x[,2]
+#    n <- length(time)
+#    ot <- order(time, event)
+#    rt <- rank(time, ties.method = "max")
+#    mt <- rank(time, ties.method = "min") - 1
+#    fact <- switch(ties.method, "logrank" = event / (n - mt),
+#                                "HL" = event/(n - rt + 1)
+#                  )   
+#    event - cumsum(fact[ot])[rt]
+#}
+
+.logrank_trafo <- function(...) {
+    if (require("coin")) return(coin::logrank_trafo(...))
+    stop("cannot load coin package")
 }
 
 ### convert response y to influence function h(y)
